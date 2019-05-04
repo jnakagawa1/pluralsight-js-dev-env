cd94140007dfde734f818e5e0be977e431ed6c1d
jnakagawa1 <jeannak@gmail.com>
2019-05-04 15:16:49 -0300

    8-Bundling

---
+++ src/webpack.config.dev.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+    ]
+  }
+}
